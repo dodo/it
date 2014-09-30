34e7d7b53a9c9a525472ec0f5fb7819352a850d2
▟ ▖▟ ▖ <dodo.the.last@gmail.com>
2014-09-30 03:52:06 +0200

    add fs core module

---
+++ lib/core/fs.lua
@@ -0,0 +1,14 @@
+local fs = {}
+
+
+function fs.read(filename)
+    local file = io.open(filename, 'r')
+    if not file then return end
+    local content = file:read('*a')
+    file:close()
+    return content
+end
+
+
+return fs
+
