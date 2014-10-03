0ef6a7917d90e1b9f8e6bc02089879438b3fd7a2
▟ ▖▟ ▖ <dodo.the.last@gmail.com>
2014-10-04 01:28:59 +0200

    add uv error helper defines

--- include/it.h
+++ include/it.h
@@ -1,11 +1,27 @@
 #ifndef IT_H
 #define IT_H
 
+#include <assert.h>
+
 #include <uv.h>
 #include <lua.h>
 
 #include "it-types.h"
 
+
+#define uvI_error(loop,msg) { \
+        uv_err_t err = uv_last_error(loop); \
+        fprintf(stderr, "internal error: "msg"\n", uv_err_name(err), uv_strerror(err)); \
+        assert(0); \
+    }
+
+#define uvI_lua_error(L,loop,msg) { \
+        uv_err_t err = uv_last_error(loop); \
+        return luaL_error(L, "internal error: "msg, uv_err_name(err), uv_strerror(err)); \
+    }
+
+
 int it_gets_cwd_lua(lua_State* L);
+
 
 #endif /* IT_H */
