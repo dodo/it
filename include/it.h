a4d633f6582247c76997a9879e4099f82489a437
▟ ▖▟ ▖ <dodo.the.last@gmail.com>
2014-10-05 19:46:17 +0200

    add error macros

--- include/it.h
+++ include/it.h
@@ -9,6 +9,15 @@
 #include "it-types.h"
 
 
+#define it_errors(msg, ...) { \
+        fprintf(stderr, "internal error: "msg"\n", ##__VA_ARGS__); \
+        assert(0); \
+    }
+
+#define luaI_error(L, msg, ...) { \
+        return luaL_error(L, "internal error: "msg, ##__VA_ARGS__); \
+    }
+
 #define uvI_error(loop,msg) { \
         uv_err_t err = uv_last_error(loop); \
         fprintf(stderr, "internal error: "msg"\n", uv_err_name(err), uv_strerror(err)); \
