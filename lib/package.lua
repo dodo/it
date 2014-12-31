a20e0ee4da06bb3dc9e12585a2c74ff693f955c9
▟ ▖▟ ▖ <dodo.the.last@gmail.com>
2014-12-31 10:38:05 +0100

    do ugly things by monkey patching require for package.loaded namespacing

--- lib/package.lua
+++ lib/package.lua
@@ -5,8 +5,8 @@ require 'ffi'
 package.path = './?/init.lua;' .. package.path
 
 -- add new path to api libs (core + plugins)
-package.apipath = _it.libdir .. 'core/?/init.lua'
-package.apipath = _it.libdir .. 'core/?.lua;' .. package.apipath
+package.apipath = _it.libdir .. 'rary/?/init.lua'
+package.apipath = _it.libdir .. 'rary/?.lua;' .. package.apipath
 
 -- load all plugin meta information
 _it.plugin = {}
@@ -17,8 +17,8 @@ for _,name in pairs({dofile(_it.libdir .. 'plugins.lua')}) do
         plugin.libdir = plugin.path .. 'lib/'
         plugin.apifile = 'lib' .. name .. '.so'
         package.apipath = package.apipath .. ';'
-            .. plugin.libdir .. 'core/?.lua;'
-            .. plugin.libdir .. 'core/?/init.lua'
+            .. plugin.libdir .. 'rary/?.lua;'
+            .. plugin.libdir .. 'rary/?/init.lua'
         _it.plugin[name] = plugin
     end
 end
@@ -50,6 +50,15 @@ local function local_loader(name)
     return loadfile(path .. name)
 end
 
+-- this uses at least 40mb!!! FIXME
+local function api_require(name) -- FIXME isnt this ugly in the stacktrace?
+    -- try to namespace all modules within it/lib/rary/
+    local apiname = name and 'it.' .. name or name
+    local success, mod = pcall(package.vanilla_require, apiname)
+    if success then return mod end
+    return package.vanilla_require(name)
+end
+
 -- add new loaders via environment
 function package.env(mode)
     if mode == 'vanilla' then
@@ -59,15 +68,18 @@ function package.env(mode)
             loaders[k] = v
         end
         package.loaders = loaders
+        require = package.vanilla_require
     elseif mode == 'it' then
         package.env('vanilla') -- remove any previously defined custom loaders
         table.insert(package.loaders, 1, local_loader)
         table.insert(package.loaders, 2, api_loader)
+        require = api_require
     end
 end
 
 -- load luarocks loader if present
 pcall(require, "luarocks.loader")
 -- default
+package.vanilla_require = require
 package.vanilla_loaders = package.loaders
 package.env('it')
