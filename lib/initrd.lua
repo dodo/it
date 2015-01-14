f21fec3d37365a9d5dfb630556a96f606229d110
▟ ▖▟ ▖ <dodo.the.last@gmail.com>
2015-01-14 20:09:44 +0100

    add support for lua debugger via --mobdebug

--- lib/initrd.lua
+++ lib/initrd.lua
@@ -61,6 +61,12 @@ if haz(process.argv, "--debug") then
     process.debug()
 end
 
+if haz(process.argv, "--mobdebug") then
+    local color = require('console').color
+    print(color.bold .. "[remote debug mode]" .. color.reset)
+    table.remove(process.argv, haz(process.argv, "--mobdebug"))
+    process.debugger = true
+end
 
 return function () -- called when finally initialized
     if #process.argv == 0 then
@@ -68,16 +74,42 @@ return function () -- called when finally initialized
         require('cli').repl()
     else
         if not fs.exists(process.argv[1]) then
-            print "script file does not exist."
+            print(string.format("script file '%s' does not exist.", process.argv[1]))
             return process.exit(1)
         end
         doc.rm()
-        local result = util.pcall(dofile, process.argv[1])
+        local result
+        if debugger then
+            io.stdout:setvbuf("no") -- disable print buffering
+            -- inject debugger loading code into script file …
+            -- … to set entry point to the script
+            -- but postbone debugging until running the actual loop
+            -- FIXME this breaks sheebangs
+            local chunk = "if process.debugger and not process.initialized then"
+                .. "require('mobdebug').start() require('mobdebug').off() "
+                .. " end; " .. fs.read(process.argv[1])
+            local code = util.pcall(loadstring, chunk, process.argv[1])
+            result = util.pcall(code)
+        else
+            result = util.pcall(dofile, process.argv[1])
+        end
+        process.initialized = true
         -- TODO test if something happened
         if type(result) ~= 'function' and process.shutdown then
             process.exit() -- normally
+            return result
         end
         process.shutdown = false
+        if type(result) == 'function' then
+            if process.debugger then
+                -- auto enable debugger just around the loop
+                return function (...)
+                    require('mobdebug').on()
+                    result(...) -- loop
+                    require('mobdebug').off()
+                end
+            end
+        end
         return result
     end
 end
